4563eb2b39fb1b356c87cd9043d56d19ac33e6e2
Daryl Robbins <daryl@robbins.name>
2013-03-09 14:38:13 -0500

    Various tweaks.

--- source/50_theme.sh
+++ source/50_theme.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# Adding formatting characters to identify file types
-alias ls="ls -F"
-
 # Command line coloring
 export CLICOLOR=1
 
 # colored grep
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;33'
-
-# colored ls
-export LSCOLORS='Gxfxcxdxdxegedabagacad'
 
 # Load colors first so they can be use in base theme
 source "${HOME}/.dotfiles/themes/colors.theme.bash"
